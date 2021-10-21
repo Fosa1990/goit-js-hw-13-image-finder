eb696b0e7c1be83c0e5696c9209a6a837d75dd93
Fosa1990 <fosa1990@gmail.com>
2021-10-21 14:58:28 +0300

    Create apiService.js

---
+++ src/js/apiService.js
@@ -0,0 +1,35 @@
+const API_KEY = '23935285-7259499af9fe05c3ca983c7fa';
+const BASE_URL = 'https://pixabay.com/api/';
+const MAIN_SEARCH_SETTINGS = '?image_type=photo&orientation=horizontal&';
+
+export default class ApiImagesService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImages() {
+    const url = `${BASE_URL}${MAIN_SEARCH_SETTINGS}q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+    return await fetch(url).then(response => {
+      this.incrementPage();
+      return response.json();
+    });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
